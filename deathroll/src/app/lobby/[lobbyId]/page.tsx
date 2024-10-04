f41969765918bca274acdabf845ae7f42febef87
WilliamBanas <banaswilliam@gmail.com>
2024-10-05 00:40:17 +0200

    Copy button fixed v2

--- deathroll/src/app/lobby/[lobbyId]/page.tsx
+++ deathroll/src/app/lobby/[lobbyId]/page.tsx
@@ -251,7 +251,7 @@ const Lobby: React.FC = () => {
 						<p className="text-2xl">{lobbyId}</p>
 						<Button
 							variant="ghost"
-							className="rounded hover:bg-transparent w-fit"
+							className="rounded hover:bg-transparent w-fit p-0"
 							onClick={copyLobbyId}
 						>
 							<Copy className="w-5" />
