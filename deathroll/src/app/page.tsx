b4928cafa5db19ed9ea32baee523d718ff5061eb
WilliamBanas <banaswilliam@gmail.com>
2024-10-10 13:32:06 +0200

    final fix rules

--- deathroll/src/app/page.tsx
+++ deathroll/src/app/page.tsx
@@ -81,70 +81,77 @@ const Home: React.FC = () => {
 	return (
 		<main className="px-6">
 			<div className="flex flex-col items-center gap-4 w-full m-auto max-w-96 mt-16">
-					<div className="bg-card border rounded px-6 py-4 w-full h-fit flex flex-col gap-3">
-						{error && <p className="text-red-500">{error}</p>}
+				<div className="bg-card border rounded px-6 py-4 w-full h-fit flex flex-col gap-3">
+					{error && <p className="text-red-500">{error}</p>}
+					<Input
+						type="text"
+						placeholder="Enter Nickname"
+						value={nickname}
+						onChange={(e) => setNickname(e.target.value)}
+						className="rounded"
+					/>
+					<Button
+						disabled={nickname === ""}
+						className={`rounded`}
+						onClick={createLobby}
+					>
+						Create lobby
+					</Button>
+					<div className="flex gap-3">
 						<Input
 							type="text"
-							placeholder="Enter Nickname"
-							value={nickname}
-							onChange={(e) => setNickname(e.target.value)}
+							placeholder="Enter Lobby ID"
+							value={lobbyId}
+							onChange={handleLobbyIdChange}
 							className="rounded"
 						/>
 						<Button
-							disabled={nickname === ""}
-							className={`rounded`}
-							onClick={createLobby}
+							disabled={nickname === "" || lobbyId === ""}
+							className={`rounded text-white bg-secondary hover:bg-secondary/50`}
+							onClick={joinLobby}
+							variant="ghost"
 						>
-							Create lobby
+							Join lobby
 						</Button>
-						<div className="flex gap-3">
-							<Input
-								type="text"
-								placeholder="Enter Lobby ID"
-								value={lobbyId}
-								onChange={handleLobbyIdChange}
-								className="rounded"
-							/>
-							<Button
-								disabled={nickname === "" || lobbyId === ""}
-								className={`rounded text-white bg-secondary hover:bg-secondary/50`}
-								onClick={joinLobby}
-								variant="ghost"
-							>
-								Join lobby
-							</Button>
-						</div>
-					</div>
-					<div className="bg-card border rounded w-full px-6 h-fit">
-						<Accordion type="single" collapsible>
-							<AccordionItem value="item-1">
-								<AccordionTrigger className="text-lg">What are the rules ?</AccordionTrigger>
-								<AccordionContent className="text-lg">
-									Each player rolls a random number between two values. The first
-									player to play has to roll a number between 1 and a value set by
-									the game host. The next player has to roll a number between 1
-									and the number the previous player rolled, and so on... The
-									first player to hit 1 loses.
-                  <br />
-                  <br />
-                  Example:
-                  <br />
-                  Default value is 100.
-                  <br />
-                  Player1 rolls between 1 and 100 and obtains 42.
-                  Player2 rolls between 1 and 42 and obtains 33.
-                  Player3 rolls between 1 and 33 and obtains 12.
-                  Player4 rolls between 1 and 12 and obtains 8.
-                  Player5 rolls between 1 and 8 and obtains 1.
-
-                  Player5 loses.
-								</AccordionContent>
-							</AccordionItem>
-						</Accordion>
 					</div>
 				</div>
-			</main>
-		);
-	};
+				<div className="bg-card border rounded w-full px-6 h-fit">
+					<Accordion type="single" collapsible>
+						<AccordionItem value="item-1">
+							<AccordionTrigger className="text-lg">
+								What are the rules ?
+							</AccordionTrigger>
+							<AccordionContent className="text-lg">
+								Each player rolls a random number between two values. The first
+								player to play has to roll a number between 1 and a value set by
+								the game host. The next player has to roll a number between 1
+								and the number the previous player rolled, and so on... The
+								first player to hit 1 loses.
+								<br />
+								<br />
+								Example:
+								<br />
+								Default value is 100.
+								<br />
+								Player1 rolls between 1 and 100 and obtains 42.
+								<br />
+								Player2 rolls between 1 and 42 and obtains 33.
+								<br />
+								Player3 rolls between 1 and 33 and obtains 12
+								<br />
+								Player4 rolls between 1 and 12 and obtains 8.
+								<br />
+								Player5 rolls between 1 and 8 and obtains 1.
+								<br />
+								<br />
+								Player5 loses.
+							</AccordionContent>
+						</AccordionItem>
+					</Accordion>
+				</div>
+			</div>
+		</main>
+	);
+};
 
 export default Home;
