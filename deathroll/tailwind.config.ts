ecd2a3ee885ae162281a1adeae7be27d5fde16b4
William Banas <banaswilliam@gmail.com>
2024-10-14 22:39:19 +0200

    azerty

--- deathroll/tailwind.config.ts
+++ deathroll/tailwind.config.ts
@@ -6,6 +6,6 @@ const config: Config = {
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	plugins: [require("tailwindcss-animate"), require("daisyui")],
+	plugins: [require("daisyui")],
 };
 export default config;
