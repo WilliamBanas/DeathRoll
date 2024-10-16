f5a15aa1fb4b54b85253c8d1d6fbffe6b09ebb9e
William Banas <banaswilliam@gmail.com>
2024-10-16 20:30:39 +0200

    erg

--- deathroll/src/components/Header.tsx
+++ deathroll/src/components/Header.tsx
@@ -1,6 +1,9 @@
+import Logo from '../app/assets/logo/Deathroll.svg'
+
 export default function Header() {
 	return (
-    <header className="w-full flex items-center justify-center border h-16 px-6 py-2">
+    <header className="w-full flex items-center justify-center h-16 px-6 py-2">
+      <Logo />
     </header>
   );
 }
