721630570a8f11e81e86cc675bb8f668ceb3a935
WilliamBanas <banaswilliam@gmail.com>
2024-10-24 16:40:52 +0200

    blabla

--- deathroll/src/app/api/keep-alive/route.ts
+++ deathroll/src/app/api/keep-alive/route.ts
@@ -11,5 +11,5 @@ export async function GET(request: Request) {
 
   console.log('Cronjob exécuté avec succès');
 
-  return NextResponse.json(null, { status: 204 });
+  return new NextResponse(null, { status: 204 });
 }
