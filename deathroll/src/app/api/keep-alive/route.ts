6c4099d875b118e107394212796852977178388c
WilliamBanas <banaswilliam@gmail.com>
2024-10-24 15:44:20 +0200

    cronjob route added

---
+++ deathroll/src/app/api/keep-alive/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get('secret');
+  const CRON_SECRET = process.env.CRON_SECRET;
+
+  if (secret !== CRON_SECRET) {
+    return NextResponse.json({ message: 'Accès interdit' }, { status: 403 });
+  }
+
+  console.log('Cronjob exécuté avec succès');
+
+  return new NextResponse(null, { status: 204 });
+}
