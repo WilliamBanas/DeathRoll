ac4e3a9266ef05c37fd682d5f8477fbffc4ef9e7
WilliamBanas <banaswilliam@gmail.com>
2024-10-25 22:23:08 +0200

    beta

--- deathroll/src/components/Header.tsx
+++ deathroll/src/components/Header.tsx
@@ -2,8 +2,13 @@ import Logo from "../app/assets/logo/Deathroll.svg"
 
 export default function Header() {
 	return (
-		<header className="w-full h-32 flex items-center justify-center  h-32 px-6 py-2">
-			<Logo  />
+		<header className="h-32 flex items-center justify-center h-32 px-6 py-2">
+      <div className="w-fit relative">
+				<Logo />
+        <div className="badge badge-error text-white absolute top-0 right-0 rotate-12 -translate-y-1/2 translate-x-1/2 animate-shake-delayed">
+          beta
+        </div>
+			</div>
 		</header>
 	);
 }
--- deathroll/tailwind.config.ts
+++ deathroll/tailwind.config.ts
@@ -7,6 +7,20 @@ const config: Config = {
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
+  theme: {
+    extend: {
+      keyframes: {
+        shakeDelayed: {
+          '0%, 92%': { transform: 'translate(50%, -50%) rotate(12deg)' },
+          '93%, 95%, 97%, 99%': { transform: 'translate(47%, -53%) rotate(12deg)' },
+          '94%, 96%, 98%, 100%': { transform: 'translate(53%, -47%) rotate(12deg)' },
+        }
+      },
+      animation: {
+        'shake-delayed': 'shakeDelayed 8s ease-in-out infinite',
+      }
+    }
+  },
 	plugins: [daisyui],
   daisyui: {
 		themes: ["black", "dark", "dracula"],
