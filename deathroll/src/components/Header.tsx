291a2929582124b0b99f08953ac2a181b1cd0870
WilliamBanas <banaswilliam@gmail.com>
2024-10-25 22:25:34 +0200

    beta 2

--- deathroll/src/components/Header.tsx
+++ deathroll/src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
 		<header className="h-32 flex items-center justify-center h-32 px-6 py-2">
       <div className="w-fit relative">
 				<Logo />
-        <div className="badge badge-error text-white absolute top-0 right-0 rotate-12 -translate-y-1/2 translate-x-1/2 animate-shake-delayed">
+        <div className="badge badge-error badge-sm text-white absolute top-0 right-0 rotate-12 -translate-y-1/2 translate-x-1/2 animate-shake-delayed ">
           beta
         </div>
 			</div>
